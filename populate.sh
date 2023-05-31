054c7676841f8178f2c8270457fbf8a3ae70545f
Mathieu Provencher <>
2023-05-30 21:00:25 -0400

    print env vars

--- populate.sh
+++ populate.sh
@@ -2,18 +2,24 @@
 set -e
 
 if [[ -z "${BASE_PATH}" ]]; then
-  echo "[INFO] BASE_PATH environment variable not set. Using ''"
+  echo "[INFO] BASE_PATH environment variable not set. Using blank value for BASE_PATH"
   BASE_PATH=""
+else
+  echo "[INFO] Using BASE_PATH=${BASE_PATH}"
 fi
 
 if [[ -z "${STAC_ASSET_GENERATOR_TIMEOUT}" ]]; then
   echo "[INFO] STAC_ASSET_GENERATOR_TIMEOUT environment variable not set. Using '30'"
   STAC_ASSET_GENERATOR_TIMEOUT="30"
+else
+  echo "[INFO] Using STAC_ASSET_GENERATOR_TIMEOUT=${STAC_ASSET_GENERATOR_TIMEOUT}"
 fi
 
 if [[ -z "${STAC_HOST}" ]]; then
   echo "[INFO] STAC_HOST environment variable not set. Using '127.0.0.1'"
   STAC_HOST="127.0.0.1"
+else
+  echo "[INFO] Using STAC_HOST=${STAC_HOST}"
 fi
 
 cd $BASE_PATH/populator
