7133cb615628bb48aa9810cb3d1d4f196f34f4a1
Deepak Chandan <deepak.chandan@me.com>
2023-09-21 23:30:10 +0200

    revised pydantic data model

--- implementations/CMIP6-UofT/add_CMIP6.py
+++ implementations/CMIP6-UofT/add_CMIP6.py
@@ -1,21 +1,17 @@
 import argparse
-import datetime as dt
+import json
 import logging
-from typing import Any, List, Literal, MutableMapping
+from datetime import datetime
+from typing import Any, Dict, List, Literal, MutableMapping
 
 import pyessv
-import pystac
 from colorlog import ColoredFormatter
-from pydantic import (
-    BaseModel,
-    Field,
-    FieldValidationInfo,
-    ValidationError,
-    field_validator,
-)
+from pydantic import AnyHttpUrl, BaseModel, Field, FieldValidationInfo, field_validator
+from typing_extensions import TypedDict
 
 from STACpopulator import STACpopulatorBase
 from STACpopulator.input import THREDDSLoader
+from STACpopulator.stac_utils import collection2enum
 
 LOGGER = logging.getLogger(__name__)
 LOGFORMAT = "  %(log_color)s%(levelname)s:%(reset)s %(blue)s[%(name)-30s]%(reset)s %(message)s"
@@ -30,46 +26,64 @@ LOGGER.propagate = False
 CV = pyessv.WCRP.CMIP6
 
 # Enum classes built from the pyessv' CV
-Activity = collection2enum(CV.activity_id)
-Experiment = collection2enum(CV.experiment_id)
+ActivityID = collection2enum(CV.activity_id)
+ExperimentID = collection2enum(CV.experiment_id)
 Frequency = collection2enum(CV.frequency)
 GridLabel = collection2enum(CV.grid_label)
-Institution = collection2enum(CV.institution_id)
+InstitutionID = collection2enum(CV.institution_id)
 # Member = collection2enum(CV.member_id)  # This is empty
-Resolution = collection2enum(CV.nominal_resolution)
+NominalResolution = collection2enum(CV.nominal_resolution)
 Realm = collection2enum(CV.realm)
-Source = collection2enum(CV.source_id)
+SourceID = collection2enum(CV.source_id)
 SourceType = collection2enum(CV.source_type)
-SubExperiment = collection2enum(CV.sub_experiment_id)
-Table = collection2enum(CV.table_id)
-Variable = collection2enum(CV.variable_id)  # This is empty
+SubExperimentID = collection2enum(CV.sub_experiment_id)
+TableID = collection2enum(CV.table_id)
+# Variable = collection2enum(CV.variable_id)  # This is empty
 
 
-class Properties(BaseModel):
+class STACAsset(BaseModel):
+    href: AnyHttpUrl
+    media_type: str
+    title: str
+    roles: List[str]
+
+
+class Properties(BaseModel, validate_assignment=True):
     """Data model for CMIP6 Controlled Vocabulary."""
 
-    activity: Activity = Field(..., alias="activity_id")
-    experiment: Experiment = Field(..., alias="experiment_id")
-    frequency: Frequency
-    grid_label: GridLabel
-    institution: Institution = Field(..., alias="institution_id")
-    resolution: Resolution = Field(..., alias="nominal_resolution")
-    realm: List[Realm] = Field(..., alias="realm")
-    source: Source = Field(..., alias="source_id")
-    source_type: List[SourceType] = Field(..., alias="source_type")
-    sub_experiment: SubExperiment | Literal["none"] = Field(..., alias="sub_experiment_id")
-    table: Table = Field(..., alias="table_id")
-    variable: Variable = str  # Field(..., alias="variable_id")
-    variant_label: str
-    initialization_index: int
-    physics_index: int
-    realization_index: int
-    forcing_index: int
-    variant_label: str
-    tracking_id: str
-    version: str = None
-    license: str = None
-    grid: str = None
+    start_datetime: datetime
+    end_datetime: datetime
+    Conventions: str = Field(..., serialization_alias="cmip6:Conventions")
+    activity_id: ActivityID = Field(..., serialization_alias="cmip6:activity_id")
+    creation_date: datetime = Field(..., serialization_alias="cmip6:creation_date")
+    data_specs_version: str = Field(..., serialization_alias="cmip6:data_specs_version")
+    experiment: str = Field(..., serialization_alias="cmip6:experiment")
+    experiment_id: ExperimentID = Field(..., serialization_alias="cmip6:experiment_id")
+    frequency: Frequency = Field(..., serialization_alias="cmip6:frequency")
+    further_info_url: AnyHttpUrl = Field(..., serialization_alias="cmip6:further_info_url")
+    grid_label: GridLabel = Field(..., serialization_alias="cmip6:grid_label")
+    institution: str = Field(..., serialization_alias="cmip6:institution")
+    institution_id: InstitutionID = Field(..., serialization_alias="cmip6:institution_id")
+    nominal_resolution: NominalResolution = Field(..., serialization_alias="cmip6:nominal_resolution")
+    realm: List[Realm] = Field(..., serialization_alias="cmip6:realm")
+    source: str = Field(..., serialization_alias="cmip6:source")
+    source_id: SourceID = Field(..., serialization_alias="cmip6:source_id")
+    source_type: List[SourceType] = Field(..., serialization_alias="cmip6:source_type")
+    sub_experiment: str | Literal["none"] = Field(..., serialization_alias="cmip6:sub_experiment")
+    sub_experiment_id: SubExperimentID | Literal["none"] = Field(..., serialization_alias="cmip6:sub_experiment_id")
+    table_id: TableID = Field(..., serialization_alias="cmip6:table_id")
+    variable_id: str = Field(..., serialization_alias="cmip6:variable_id")
+    variant_label: str = Field(..., serialization_alias="cmip6:variant_label")
+    initialization_index: int = Field(..., serialization_alias="cmip6:initialization_index")
+    physics_index: int = Field(..., serialization_alias="cmip6:physics_index")
+    realization_index: int = Field(..., serialization_alias="cmip6:realization_index")
+    forcing_index: int = Field(..., serialization_alias="cmip6:forcing_index")
+    tracking_id: str = Field(..., serialization_alias="cmip6:tracking_id")
+    version: str = Field(..., serialization_alias="cmip6:version")
+    product: str = Field(..., serialization_alias="cmip6:product")
+    license: str = Field(..., serialization_alias="cmip6:license")
+    grid: str = Field(..., serialization_alias="cmip6:grid")
+    mip_era: str = Field(..., serialization_alias="cmip6:mip_era")
 
     @field_validator("initialization_index", "physics_index", "realization_index", "forcing_index", mode="before")
     @classmethod
@@ -84,14 +98,29 @@ class Properties(BaseModel):
         """Split string into list."""
         return v.split(" ")
 
+    @field_validator("version")
+    @classmethod
+    def validate_version(cls, v: str, info: FieldValidationInfo):
+        assert v[0] == "v", "Version string should begin with a lower case 'v'"
+        assert v[1:].isdigit(), "All characters in version string, except first, should be digits"
+        return v
+
+
+class Geometry(TypedDict):
+    type: str
+    coordinates: List[List[List[float]]]
+
 
 class STACItem(BaseModel):
-    start_datetime: dt.datetime
-    end_datetime: dt.datetime
+    id: str
+    geometry: Geometry
+    bbox: List[float]
+    properties: Properties
+    assets: Dict[str, STACAsset]
 
 
-def make_cmip6_id(attrs: MutableMapping[str, Any]) -> str:
-    """Return unique ID for CMIP6 data collection (multiple variables)."""
+def make_cmip6_item_id(attrs: MutableMapping[str, Any]) -> str:
+    """Return a unique ID for CMIP6 data item."""
     keys = [
         "activity_id",
         "institution_id",
@@ -99,6 +128,7 @@ def make_cmip6_id(attrs: MutableMapping[str, Any]) -> str:
         "experiment_id",
         "variant_label",
         "table_id",
+        "variable_id",
         "grid_label",
     ]
     return "_".join(attrs[k] for k in keys)
@@ -117,37 +147,49 @@ class CMIP6populator(STACpopulatorBase):
         """
 
         data_loader = THREDDSLoader(thredds_catalog_url)
-
-        for name, item in data_loader:
-            # self.create_stac_item(name, item)
-            print(name)
         super().__init__(stac_host, data_loader, config_filename)
 
     def handle_ingestion_error(self, error: str, item_name: str, item_data: MutableMapping[str, Any]):
         pass
 
     def create_stac_item(self, item_name: str, item_data: MutableMapping[str, Any]) -> MutableMapping[str, Any]:
-        # TODO: next step is to implement this
+        """Creates the STAC item.
+
+        :param item_name: name of the STAC item. Interpretation of name is left to the input loader implementation
+        :type item_name: str
+        :param item_data: dictionary like representation of all information on the item
+        :type item_data: MutableMapping[str, Any]
+        :return: _description_
+        :rtype: MutableMapping[str, Any]
+        """
+
         attrs = item_data["attributes"]
         meta = item_data["groups"]["CFMetadata"]["attributes"]
 
-        # Create STAC item geometry from CFMetadata
-        item = dict(
-            id=make_cmip6_id(attrs),
+        props = Properties(
+            attrs,
+            start_datetime=meta["time_coverage_start"],
+            end_datetime=meta["time_coverage_end"],
+        )
+
+        a = STACAsset(
+            href=item_data["access_urls"]["HTTPServer"],
+            media_type="application/netcdf",
+            title="HTTP Server",
+            roles=["data"],
+        )
+
+        item = STACItem(
+            id="sdfdd",
+            properties=props,
             geometry=THREDDSLoader.ncattrs_to_geometry(meta),
             bbox=THREDDSLoader.ncattrs_to_bbox(meta),
-            properties=Properties(**attrs).model_dump(),
-            datetime=None,
+            assets={"http": a},
         )
 
-        item.update(
-            STACItem(
-                start_datetime=meta["time_coverage_start"],
-                end_datetime=meta["time_coverage_end"],
-            ).model_dump()
-        )
+        stac_item_json = json.loads(item.model_dump_json(by_alias=True))
 
-        return pystac.Item(**item)
+        return stac_item_json
 
     def validate_stac_item_cv(self, data: MutableMapping[str, Any]) -> bool:
         # Validation is done at the item creating stage, using the Properties class.
@@ -163,4 +205,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     LOGGER.info(f"Arguments to call: {args}")
     c = CMIP6populator(args.stac_host, args.thredds_catalog_URL, args.config_file)
-    c.ingest()
+    # c.ingest()
