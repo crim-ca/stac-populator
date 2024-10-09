7c28fa5f9a12e4fb4e564e91a8553f30ccde8c3f
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2024-10-09 13:11:13 -0400

    backport syntax for python < 3.12

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,7 +15,7 @@ def run_cli(*args: str, **kwargs: Mapping) -> subprocess.CompletedProcess:
 
 @pytest.fixture(scope="session")
 def populator_help_pattern():
-    return re.compile(f"\{{({',?|'.join([imp.replace('.', '\\.') for imp in implementations.__all__])},?)+\}}")
+    return re.compile(f"{{({',?|'.join([imp.replace('.', '\\.') for imp in implementations.__all__])},?)+}}")
 
 
 def test_help():
