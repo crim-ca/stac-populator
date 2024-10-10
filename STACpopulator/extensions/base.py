f1c568359654747e2160f3b43004998da6c39b63
David Huard <huard.david@ouranos.ca>
2024-10-10 15:24:16 -0400

    work on extension abstraction and cordex example

---
+++ STACpopulator/extensions/base.py
@@ -0,0 +1,278 @@
+from datetime import datetime
+import json
+import jsonschema
+from typing import Any, Dict, Generic,    TypeVar, Union, cast, Optional
+from pydantic import (BaseModel, create_model, Field, FilePath, field_validator, model_validator, HttpUrl, ConfigDict,
+                      PrivateAttr)
+import pystac
+from pystac.extensions import item_assets
+from pystac.extensions.base import (
+    ExtensionManagementMixin,
+    PropertiesExtension,
+    SummariesExtension,
+)
+from pystac.extensions.base import S  # generic pystac.STACObject
+from STACpopulator.models import AnyGeometry, GeoJSONPolygon
+from STACpopulator.stac_utils import (
+    ServiceType,
+    ncattrs_to_bbox,
+    ncattrs_to_geometry,
+)
+import types
+
+
+T = TypeVar("T", pystac.Collection, pystac.Item, pystac.Asset, item_assets.AssetDefinition)
+
+
+"""
+# Context
+
+What we have:
+  - `Loader`, which returns attributes.
+  - An external json schema describing a subset of the attributes returned by the Loader. This schema might preclude
+  additional properties, so it cannot be applied wholesale to the Loader's output.
+  - `data model` describing the content we want included in the catalog. It includes a subset of the schema properties,
+  as well as additional attributes desired by the catalog admins.
+
+Desiderata:
+  - Not having to replicate existing validation logic in the schema
+  - Not having to create a modified schema
+  - Being able to supplement the schema validation by pydantic validation logic
+  - Streamline the creation of new data models (reduce boilerplate, allow subclassing)
+  - Developer-friendly validation error messages
+
+
+How-to:
+  - Instructions to create basic datamodel from schema (codegen)
+  
+  
+  
+"""
+class DataModel(BaseModel):
+    """Base class for dataset properties going into the catalog.
+
+    Subclass this with attributes.
+    """
+    # Ideally, the catalog properties would be described by a jsonschema.
+    _prefix: str = PrivateAttr()
+
+    # URI of the json schema to validate against.
+    _schema_uri: FilePath = PrivateAttr(None)
+
+    # List of properties not meant to be validated by json schema.
+    _schema_exclude: list[str] = PrivateAttr([])
+
+    model_config = ConfigDict(populate_by_name=True, extra="ignore")
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_jsonschema(cls, data):
+        """Validate the data model against the json schema, if given."""
+        # Load schema
+        uri =  cls._schema_uri.default
+        if uri is not None:
+            schema = json.load(open(uri))
+            validator_cls = jsonschema.validators.validator_for(schema)
+            validator_cls.check_schema(schema)
+            validator = validator_cls(schema)
+
+            attrs = {f"{cls._prefix.default}:{k}": v for (k,v) in data.items() if k not in cls._schema_exclude.default}
+            errors = list(validator.iter_errors(attrs))
+            if errors:
+                raise ValueError(errors)
+
+        return data
+
+
+class THREDDSCatalogDataModel(BaseModel):
+    """Base class ingesting attributes loaded by `THREDDSLoader` and creating a STAC item.
+
+    This is meant to be subclassed for each extension.
+
+    It includes two validation mechanisms:
+     - pydantic validation using type hints, and
+     - json schema validation.
+    """
+    properties: DataModel
+
+    # Fields required by STAC item creation
+    geometry: GeoJSONPolygon
+    bbox: list[float]
+    start_datetime: datetime
+    end_datetime: datetime
+
+    # Field for asset creation
+    access_urls: Dict[str, HttpUrl]
+
+    # Private attributes used to validate schema properties
+    model_config = ConfigDict(populate_by_name=True, extra="ignore")
+
+    @classmethod
+    def from_data(cls, data):
+        """Instantiate class from data provided by THREDDS Loader."""
+        return cls(properties=data["attributes"],
+                   start_datetime=data["groups"]["CFMetadata"]["attributes"]["time_coverage_start"],
+                   end_datetime=data["groups"]["CFMetadata"]["attributes"]["time_coverage_end"],
+                   geometry=ncattrs_to_geometry(data),
+                   bbox=ncattrs_to_bbox(data),
+                   access_urls=data["access_urls"]
+                   )
+
+    @property
+    def uid(self) -> str:
+        """Return a unique ID. When subclassing, use a combination of properties uniquely identifying a dataset."""
+        import uuid
+        return str(uuid.uuid4())
+
+    @field_validator("access_urls")
+    @classmethod
+    def validate_access_urls(cls, value):
+        assert len(set(["HTTPServer", "OPENDAP"]).intersection(value.keys())) >= 1, (
+            "Access URLs must include HTTPServer or OPENDAP keys.")
+        return value
+
+    def stac_item(self) -> "pystac.Item":
+        item = pystac.Item(
+            id=self.uid,
+            geometry=self.geometry.model_dump(),
+            bbox=self.bbox,
+            properties={
+                "start_datetime": self.start_datetime,
+                "end_datetime": self.end_datetime,
+            },
+            datetime=None,
+        )
+        ExtSubCls = meta_extension(self.properties._prefix, schema_uri=self.properties._schema_uri)
+        item_ext = ExtSubCls.ext(item, add_if_missing=True)
+        item_ext.apply(self.properties.model_dump(by_alias=True))
+        return item
+
+
+def meta_extension(name, schema_uri):
+        cls_name = f"{name.upper()}Extension"
+
+        bases = (MetaExtension,
+                 Generic[T],
+                 PropertiesExtension,
+                 ExtensionManagementMixin[Union[pystac.Asset, pystac.Item, pystac.Collection]]
+                 )
+
+        attrs = {"schema_name": name, "schema_uri": schema_uri}
+        return types.new_class(name=cls_name, bases=bases, kwds=None, exec_body=lambda ns: ns.update(attrs))
+
+
+def extend_type(stac, cls, ext):
+    cls_name = f"{stac.__name__ }{ext.__name__}"
+    return types.new_class(cls_name, (cls, ext), {}, lambda ns: ns)
+
+
+class MetaExtension:
+    schema_name: str
+    schema_uri: str
+
+    @property
+    def name(self) -> str:
+        return self.schema_name
+
+    def apply(self, properties: dict[str, Any]) -> None:
+        """Applies CMIP6 Extension properties to the extended
+        :class:`~pystac.Item` or :class:`~pystac.Asset`.
+        """
+        for prop, val in properties.items():
+            self._set_property(prop, val)
+
+    @classmethod
+    def get_schema_uri(cls) -> str:
+        return cls.schema_uri
+
+    @classmethod
+    def has_extension(cls, obj: S):
+        # FIXME: this override should be removed once an official and versioned schema is released
+        # ignore the original implementation logic for a version regex
+        # since in our case, the VERSION_REGEX is not fulfilled (ie: using 'main' branch, no tag available...)
+        ext_uri = cls.get_schema_uri()
+        return obj.stac_extensions is not None and any(uri == ext_uri for uri in obj.stac_extensions)
+
+    @classmethod
+    def ext(cls, obj: T, add_if_missing: bool = False) -> "Extension[T]":
+        """Extends the given STAC Object with properties from the
+        :stac-ext:`Extension`.
+
+        This extension can be applied to instances of :class:`~pystac.Item` or
+        :class:`~pystac.Asset`.
+
+        Raises:
+
+            pystac.ExtensionTypeError : If an invalid object type is passed.
+        """
+        cls_map = {pystac.Item: MetaItemExtension}
+
+        for key, meta in cls_map.items():
+            if isinstance(obj, key):
+                cls.ensure_has_extension(obj, add_if_missing)
+                kls = extend_type(key, meta, cls[key])
+
+                return cast(cls[T], kls(obj))
+        else:
+            raise pystac.ExtensionTypeError(cls._ext_error_message(obj))
+
+
+class MetaItemExtension:
+    """A concrete implementation of :class:`CMIP6Extension` on an :class:`~pystac.Item`
+    that extends the properties of the Item to include properties defined in the
+    :stac-ext:`Extension`.
+
+    This class should generally not be instantiated directly. Instead, call
+    :meth:`CMIP6Extension.ext` on an :class:`~pystac.Item` to extend it.
+    """
+    def __init__(self, item: pystac.Item):
+        self.item = item
+        self.properties = item.properties
+
+    def get_assets(
+        self,
+        service_type: Optional[ServiceType] = None,
+    ) -> dict[str, pystac.Asset]:
+        """Get the item's assets where eo:bands are defined.
+
+        Args:
+            service_type: If set, filter the assets such that only those with a
+                matching :class:`~STACpopulator.stac_utils.ServiceType` are returned.
+
+        Returns:
+            Dict[str, Asset]: A dictionary of assets that match ``service_type``
+                if set or else all of this item's assets were service types are defined.
+        """
+        return {
+            key: asset
+            for key, asset in self.item.get_assets().items()
+            if (service_type is ServiceType and service_type.value in asset.extra_fields)
+            or any(ServiceType.from_value(field, default=None) is ServiceType for field in asset.extra_fields)
+        }
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} Item id={self.item.id}>"
+
+
+def schema_properties(schema: dict) -> list[str]:
+    """Return the list of properties described by schema."""
+    out = []
+    for key, val in schema["properties"].items():
+        prefix, name = key.split(":") if ":" in key else (None, key)
+        out.append(name)
+    return out
+
+
+def model_from_schema(model_name, schema: dict):
+    """Create pydantic BaseModel from JSON schema."""
+    type_map = {"string": str, "number": float, "integer": int, "boolean": bool, "array": list, "object": dict,
+                None: Any}
+
+    fields = {}
+    for key, val in schema["properties"].items():
+        prefix, name = key.split(":") if ":" in key else (None, key)
+        typ = type_map[val.get("type")]
+        default = ... if key in schema["required"] else None
+        fields[name] = (typ, Field(default, alias=key))
+    return create_model(model_name, **fields)
+
---
+++ STACpopulator/extensions/cordex6.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from pathlib import Path
+from pydantic import BaseModel, Field, FilePath
+from datetime import datetime
+
+from importlib import reload
+import STACpopulator.extensions.base
+reload(STACpopulator.extensions.base)
+from STACpopulator.extensions.base import THREDDSCatalogDataModel, DataModel
+
+
+# This is generated using datamodel-codegen + manual edits
+class CordexCmip6(DataModel):
+    # Fields from schema
+    activity_id: str = Field(..., alias='cordex6:activity_id')
+    contact: str = Field(..., alias='cordex6:contact')
+    # Conventions: str = Field(..., alias='cordex6:Conventions')
+    creation_date: datetime = Field(..., alias='cordex6:creation_date')
+    domain_id: str = Field(..., alias='cordex6:domain_id')
+    domain: str = Field(..., alias='cordex6:domain')
+    driving_experiment_id: str = Field(..., alias='cordex6:driving_experiment_id')
+    driving_experiment: str = Field(..., alias='cordex6:driving_experiment')
+    driving_institution_id: str = Field(..., alias='cordex6:driving_institution_id')
+    driving_source_id: str = Field(..., alias='cordex6:driving_source_id')
+    driving_variant_label: str = Field(..., alias='cordex6:driving_variant_label')
+    frequency: str = Field(..., alias='cordex6:frequency')
+    grid: str = Field(..., alias='cordex6:grid')
+    institution: str = Field(..., alias='cordex6:institution')
+    institution_id: str = Field(..., alias='cordex6:institution_id'    )
+    license: str = Field(..., alias='cordex6:license')
+    mip_era: str = Field(..., alias='cordex6:mip_era')
+    product: str = Field(..., alias='cordex6:product')
+    project_id: str = Field(..., alias='cordex6:project_id')
+    source: str = Field(..., alias='cordex6:source')
+    source_id: str = Field(..., alias='cordex6:source_id')
+    source_type: str = Field(..., alias='cordex6:source_type')
+    tracking_id: str = Field(..., alias='cordex6:tracking_id')
+    variable_id: str = Field(..., alias='cordex6:variable_id')
+    version_realization: str = Field(..., alias='cordex6:version_realization')
+
+    # Extra fields
+    external_variables: str | list[str]
+
+    _prefix: str = "cordex6"
+    _schema_uri: FilePath = Path(__file__).parent / "schemas" / "cordex6" / "cmip6-cordex-global-attrs-schema.json"
+
+
+# Customize the THREDDSCatalogDataModel
+class Cordex6DataModel(THREDDSCatalogDataModel):
+    properties: CordexCmip6
+
+    @property
+    def uid(self) -> str:
+        """Return a unique ID for CMIP6 data item."""
+        keys = [
+            "activity_id",
+            "driving_institution_id",
+            "driving_source_id",
+            "institution_id",
+            "source_id",
+            "driving_experiment_id",
+            "driving_variant_label",
+            "variable_id",
+            "grid",
+        ]
+        name = "_".join(getattr(self.properties, k) for k in keys)
+        return name
+
+
+
+
+def get_test_data():
+    import requests
+    from siphon.catalog import TDSCatalog
+    import xncml
+    from STACpopulator.stac_utils import numpy_to_python_datatypes
+
+    cat = TDSCatalog("https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/catalog/birdhouse/disk2/ouranos/CORDEX/CMIP6/DD/NAM-12/OURANOS/MPI-ESM1-2-LR/ssp370/r1i1p1f1/CRCM5/v1-r1/day/tas/v20231208/catalog.html")
+
+    if cat.datasets.items():
+        for item_name, ds in cat.datasets.items():
+            url = ds.access_urls["NCML"]
+            r = requests.get(url)
+            attrs = xncml.Dataset.from_text(r.text).to_cf_dict()
+            attrs["attributes"] = numpy_to_python_datatypes(attrs["attributes"])
+            attrs["access_urls"] = ds.access_urls
+            return attrs
