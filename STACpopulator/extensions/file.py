c462ecd99a9fb74626db7140da386e320f22071b
Henri Aïdasso <henri.aidasso@crim.ca>
2025-10-15 14:47:30 -0400

    fix: file helper updated

--- STACpopulator/extensions/file.py
+++ STACpopulator/extensions/file.py
@@ -1,6 +1,6 @@
+"""FileHelper module."""
+
 import functools
-import hashlib
-import io
 from typing import Dict, TypeVar
 
 import pystac
@@ -9,67 +9,57 @@ from pystac.extensions.file import FileExtension
 
 from STACpopulator.extensions.base import ExtensionHelper
 
+# Constants
 T = TypeVar("T", pystac.Asset, pystac.Link)
+HTTP_SERVER_ASSET_KEY = "HTTPServer"
+OPEN_DAP_ASSET_KEY = "OpenDAP"
 
 
 class FileHelper(ExtensionHelper):
     """Helper to handle file info from elements of types Asset and Link."""
 
     access_urls: Dict[str, str]
-    _local_file = None
-    _header_size = None
+    _session = requests.Session()
 
     def apply(self, item: pystac.Item, add_if_missing: bool = True) -> T:
         """Apply the FileExtension to an asset."""
-        if self._local_file is None:
-            self.load_file()
-
         # FIXME: This extension is applicable to Assets and Links.
-        # Right now it is only applied to the HTTPServer asset to avoid
-        # too much load/latence when running the populator
-        asset = item.assets["HTTPServer"]
+        # Currently only applied to the HTTPServer asset to avoid heavy load during populator run
+        asset = item.assets[HTTP_SERVER_ASSET_KEY]
         file_ext = FileExtension.ext(asset, add_if_missing=add_if_missing)
         file_ext.apply(
-            byte_order=None,  # NOTE: Appear to available for each variable. Not sure what would be the value for the whole file.
-            checksum=self.checksum,
+            byte_order=None,  # NOTE: Appears to be variable-related. Unclear what would be the value for the whole file.
             header_size=self.header_size,
             size=self.size,
-            values=None,  # NOTE: deprecated
-            local_path=None,  # NOTE: might not be relevant
+            checksum=None,  # NOTE: Should be made available in the metadata on THREDDS catalog
+            values=None,  # NOTE: Deprecated field
+            local_path=None,  # NOTE: Seems to be irrelevant
         )
-
-    def load_file(self) -> None:
-        """Download the current file into temporary directory."""
-        if self._local_file is not None:
-            return
-        response = requests.get(self.access_urls["HTTPServer"], stream=True)
-        response.raise_for_status()
-
-        # Read the first 64 KB for header/metadata (NetCDF4/HDF5 header)
-        head_chunk = response.raw.read(64 * 1024)
-        self._header_size = len(head_chunk)
-
-        # Initialize in-memory buffer with header
-        buffer = io.BytesIO(head_chunk)
-
-        # Read the rest of the stream and append to buffer while counting
-        for chunk in response.iter_content(chunk_size=8192):
-            buffer.write(chunk)
-
-        # Store all data in memory
-        self._local_file = buffer.getvalue()
 
     @functools.cached_property
     def size(self) -> int:
         """Return file size in bytes."""
-        return len(self._local_file)
-
-    @functools.cached_property
-    def checksum(self) -> str:
-        """Return file sha256 checksum."""
-        return hashlib.sha256(self._local_file).hexdigest()
+        if HTTP_SERVER_ASSET_KEY not in self.access_urls:
+            return 0
+        res = self._session.head(self.access_urls[HTTP_SERVER_ASSET_KEY])
+        res.raise_for_status()
+        return int(res.headers.get("Content-Length", None))
 
     @functools.cached_property
     def header_size(self) -> int:
-        """Return the header size of the netCDF (.nc) file in bytes."""
-        return self._header_size
+        """Return the header size of the netCDF file in bytes."""
+        # FIXME: Implementation to be discussed to confirm compliance with header size definition
+        if OPEN_DAP_ASSET_KEY not in self.access_urls:
+            return 0
+
+        header_size = 0
+        # <url>.dds returns variable and dimension structure
+        # <url>.das returns attributes and metadata
+        for end in [".dds", ".das"]:
+            url = self.access_urls[OPEN_DAP_ASSET_KEY] + end
+            res = self._session.get(url, stream=True)
+            res.raise_for_status()
+
+            bytes_count: int = sum(len(chunk) for chunk in res.iter_content(chunk_size=8192))
+            header_size += bytes_count
+        return header_size
