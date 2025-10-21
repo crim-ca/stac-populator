4597fe9023cc30188e1a5db6d6a7e7c3fe877d84
Henri Aïdasso <ahenrij@gmail.com>
2025-10-21 17:38:57 -0400

    fix: rdps tests updated

--- tests/test_rdps.py
+++ tests/test_rdps.py
@@ -2,27 +2,28 @@
 
 import json
 
-from STACpopulator.extensions.thredds import THREDDSCatalogDataModel
+from STACpopulator.extensions.hrdps import HRDPSDataModel
+from STACpopulator.extensions.rdps import RDPSDataModel
 
 
 def test_rdps():
     attrs = json.load(open("tests/data/rdps.json"))
-    model = THREDDSCatalogDataModel.from_data(attrs)
+    model = RDPSDataModel.from_data(attrs)
     item = model.stac_item()
-    assert set(model._helpers) == {"thredds", "datacube"}
+    assert set(model._helpers) == {"thredds", "datacube", "cf", "file"}
     assert item["id"] == "birdhouse__testdata__HRDPS__RDPS_sample__2024010100_000.nc"
     assert "TD" in item["properties"]["cube:variables"]
 
 
 def test_hrdps():
     attrs = json.load(open("tests/data/hrdps_sfc.json"))
-    model = THREDDSCatalogDataModel.from_data(attrs)
+    model = HRDPSDataModel.from_data(attrs)
     item = model.stac_item()
     assert "HRDPS_P_PR_SFC" in item["properties"]["cube:variables"]
     assert item["properties"]["cube:dimensions"].keys() == {"time", "rlat", "rlon"}
 
     attrs = json.load(open("tests/data/hrdps_p_tt.json"))
-    model = THREDDSCatalogDataModel.from_data(attrs)
+    model = HRDPSDataModel.from_data(attrs)
     item = model.stac_item()
     assert "HRDPS_P_TT_10000" in item["properties"]["cube:variables"]
     assert item["properties"]["cube:dimensions"].keys() == {"time", "rlat", "rlon"}
