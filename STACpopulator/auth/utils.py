b6cdb4d095228c73c2edae2a6c2c666403cfd30b
Henri Aïdasso <ahenrij@gmail.com>
2025-12-19 10:44:41 -0500

    feat: add auth handler utility functions

--- STACpopulator/auth/utils.py
+++ STACpopulator/auth/utils.py
@@ -1,15 +1,15 @@
+import importlib
 import inspect
 import logging
-from types import MappingProxyType
-from typing import Any, Callable, Dict, List, Optional, Type, Union
+import os
+import re
+from typing import Any, Callable, Dict, Optional, Type, Union
 
 import requests
 from requests import Response
-from requests.structures import CaseInsensitiveDict
 from typing_extensions import Unpack
 
 from STACpopulator.auth.typedefs import (
-    AnyHeadersContainer,
     AnyRequestMethod,
     AnyValueType,
     RequestOptions,
@@ -22,105 +22,64 @@ RequestCachingKeywords = Dict[str, AnyValueType]
 RequestCachingFunction = Callable[[AnyRequestMethod, str, RequestCachingKeywords], Response]
 
 
-def _request_call(method: AnyRequestMethod, url: str, kwargs: RequestCachingKeywords) -> Response:
-    """Request operation employed by :func:`request_extra` without caching."""
-    with requests.Session() as request_session:
-        resp = request_session.request(method, url, **kwargs)
-    return resp
-
-
-def request_extra(
-    method: AnyRequestMethod, url: str, ssl_verify: bool = True, **request_kwargs: Unpack[RequestOptions]
+def make_request(
+    method: AnyRequestMethod,
+    url: str,
+    ssl_verify: bool = True,
+    **request_kwargs: Unpack[RequestOptions],
 ) -> Response:
-    """
-    Make an HTTP request with additional request options.
+    """Make an HTTP request with additional request options.
 
-    :param method: HTTP method to use (e.g., 'GET', 'POST'). Type should be compatible with `AnyRequestMethod`.
-    :param url: Target URL for the request.
-    :param ssl_verify: Whether to verify SSL certificates (default: True). Overrides `verify` in `requests`.
-    :param request_kwargs: Additional keyword arguments to pass to the underlying request,
-                           e.g., headers, data, params, json, etc.
-    :return: The response from `_request_call`.
+    Parameters
+    ----------
+    method: AnyRequestMethod
+        The HTTP method to use (e.g., 'GET', 'POST').
+    url: str
+        The target URL for the request.
+    ssl_verify: bool, optional
+        Whether to verify SSL certificates (default is True). Overrides the `verify` parameter in `requests`.
+    request_kwargs: dict, optional
+        Additional keyword arguments to pass to the underlying request, such as headers, data, params, json, etc.
+
+    Returns
+    -------
+    Response
+        The response object returned by the request, typically containing status, headers, and content.
     """
     request_kwargs.setdefault("timeout", 5)
     request_kwargs.setdefault("verify", ssl_verify)
     # remove leftover options unknown to requests method in case of multiple entries
     known_req_opts = set(inspect.signature(requests.Session.request).parameters)
-    known_req_opts -= {"url", "method"}  # add as unknown to always remove them since they are passed by arguments
+    known_req_opts -= {
+        "url",
+        "method",
+    }  # add as unknown to always remove them since they are passed by arguments
     for req_opt in set(request_kwargs) - known_req_opts:
         request_kwargs.pop(req_opt)
     request_args = (method, url, request_kwargs)
     return _request_call(*request_args)
 
 
-def get_header(
-    header_name: str,
-    header_container: AnyHeadersContainer,
-    default: Optional[Union[str, List[str]]] = None,
-    pop: bool = False,
-    concat: bool = False,
-) -> Optional[Union[str, List[str]]]:
-    """
-    Find the specified header within a header container.
-
-    Retrieves :paramref:`header_name` by fuzzy match (independently of upper/lower-case and underscore/dash) from
-    various framework implementations of *Headers*.
-
-    :param header_name: Header to find.
-    :param header_container: Where to look for :paramref:`header_name`.
-    :param default: Returned value if :paramref:`header_container` is invalid or :paramref:`header_name` is not found.
-    :param pop: Remove the matched header(s) by name from the input container.
-    :param concat:
-        Allow parts of the header name to be concatenated without hyphens/underscores.
-        This can be the case in some :term:`S3` responses.
-        Disabled by default to avoid unexpected mismatches, notably for shorter named headers.
-    :returns: Found header if applicable, or the default value.
-    """
-
-    def fuzzy_name(_name: str) -> str:
-        return _name.lower().replace("-", "_")
-
-    def concat_name(_name: str) -> str:
-        return _name.replace("-", " ").replace("_", " ").capitalize().replace(" ", "")
-
-    if header_container is None:
-        return default
-    headers = header_container
-    if isinstance(
-        headers,
-        (CaseInsensitiveDict, MappingProxyType),
-    ):
-        headers = dict(headers)
-    if isinstance(headers, dict):
-        headers = header_container.items()
-    header_name = fuzzy_name(header_name)
-    for i, (h, v) in enumerate(list(headers)):
-        if fuzzy_name(h) == header_name or (concat and concat_name(h) == concat_name(header_name)):
-            if pop:
-                if isinstance(header_container, dict):
-                    del header_container[h]
-                else:
-                    del header_container[i]
-            return v
-    return default
+def _request_call(method: AnyRequestMethod, url: str, kwargs: RequestCachingKeywords) -> Response:
+    """Request operation employed by :func:`request_extra` without caching."""
+    with requests.Session() as session:
+        res = session.request(method, url, **kwargs)
+    return res
 
 
 def fully_qualified_name(obj: Union[Any, Type[Any]]) -> str:
-    """
-    Get the full path definition of the object to allow finding and importing it.
+    """Get the full path definition of the object to allow finding and importing it.
 
-    For classes, functions and exceptions, the following format is returned:
+    For classes, functions, and exceptions, the returned format is:
 
     .. code-block:: python
-
         module.name
 
     The ``module`` is omitted if it is a builtin object or type.
 
-    For methods, the class is also represented, resulting in the following format:
+    For methods, the owning class is also included, resulting in:
 
     .. code-block:: python
-
         module.class.name
     """
     if inspect.ismethod(obj):
@@ -129,3 +88,66 @@ def fully_qualified_name(obj: Union[Any, Type[Any]]) -> str:
     if "builtins" in getattr(cls, "__module__", "builtins"):  # sometimes '_sitebuiltins'
         return cls.__name__
     return ".".join([cls.__module__, cls.__name__])
+
+
+def import_target(target: str, default_root: Optional[str] = None) -> Optional[Any]:
+    """Import a target resource class or function from a Python script as module or directly from a module reference.
+
+    The Python script does not need to be defined within a module directory (i.e.: with ``__init__.py``).
+    Files can be imported from virtually anywhere. To avoid name conflicts in generated module references,
+    each imported target employs its full escaped file path as module name.
+
+    The following target formats are supported:
+
+    .. code-block:: text
+        "path/to/script.py:function"
+        "path/to/script.py:Class"
+        "module.path.function"
+        "module.path.Class"
+
+    Parameters
+    ----------
+    target : str
+        Resource to be imported, specified as a module path or file path with
+        an optional attribute reference.
+    default_root : str, optional
+        Root directory to use when resolving relative file paths. Defaults to `None`.
+
+    Returns
+    -------
+    Any or None
+        The imported class or function if found; otherwise, `None`.
+    """
+    if ":" in target:
+        mod_path, target = target.rsplit(":", 1)
+        if not mod_path.startswith("/"):
+            if default_root:
+                mod_root = default_root
+            else:
+                mod_root = os.path.abspath(os.path.curdir)
+            if not os.path.isdir(mod_root):
+                LOGGER.warning("Cannot import relative target, root directory not found: [%s]", mod_root)
+                return None
+            mod_path = os.path.join(mod_root, mod_path)
+        mod_path = os.path.abspath(mod_path)
+        if not os.path.isfile(mod_path):
+            LOGGER.warning("Cannot import target reference, file not found: [%s]", mod_path)
+            return None
+        mod_name = re.sub(r"\W", "_", mod_path)
+        mod_spec = importlib.util.spec_from_file_location(mod_name, mod_path)
+    else:
+        mod_name = target
+        mod_path, target = target.rsplit(".", 1)
+        mod_spec = importlib.util.find_spec(mod_path)
+
+    if not mod_spec:
+        LOGGER.warning(
+            "Cannot import target reference [%s], not found in file: [%s]",
+            mod_name,
+            mod_path,
+        )
+        return None
+
+    mod = importlib.util.module_from_spec(mod_spec)
+    mod_spec.loader.exec_module(mod)
+    return getattr(mod, target, None)
