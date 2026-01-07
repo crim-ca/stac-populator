0924779398233d73037ff79c8cfd12b9ce6647b8
Henri Aïdasso <ahenrij@gmail.com>
2026-01-07 14:37:04 -0500

    fix: cmd options and help strings updated

--- STACpopulator/request_utils.py
+++ STACpopulator/request_utils.py
@@ -12,11 +12,8 @@ LOGGER = logging.getLogger(__name__)
 
 def add_request_options(parser: argparse.ArgumentParser) -> None:
     """Add arguments to a parser to allow update of a request session definition used across a populator procedure."""
-    auth_handlers = "".join(
-        [
-            f"- `{fully_qualified_name(handler)}`\n\n"
-            for handler in [BasicAuthHandler, BearerAuthHandler, CookieAuthHandler]
-        ]
+    auth_handlers = " | ".join(
+        [f"{fully_qualified_name(handler)}" for handler in [BasicAuthHandler, BearerAuthHandler, CookieAuthHandler]]
     )
 
     parser.add_argument("--cert", type=argparse.FileType(), help="Path to a certificate file to use.")
@@ -29,35 +26,33 @@ def add_request_options(parser: argparse.ArgumentParser) -> None:
         help="Disable SSL verification (not recommended unless for development/test servers).",
     )
     parser.add_argument(
-        "-aC",
+        "-c",
         "--auth-class",
-        "--auth-handler",
         dest="auth_handler",
         metavar="AUTH_HANDLER_CLASS",
         action=ValidateAuthHandlerAction,
         help=(
-            "Script or module path reference to class implementation to handle inline request authentication.\n\n"
-            "Format ``path/to/script.py:module.AuthHandlerClass`` "
-            "or ``installed.module.AuthHandlerClass`` is expected.\n\n"
-            f"Utility definitions are available as:\n\n{auth_handlers}\n"
-            "Custom implementations are allowed for more advanced use cases."
+            "Script or module path reference to a class that handles inline request authentication."
+            "The expected format is path/to/script.py:module.AuthHandlerClass or installed.module.AuthHandlerClass."
+            f"Following built-in implementations are available: {auth_handlers}."
+            "Custom implementations may be provided for more advanced use cases."
         ),
     )
     parser.add_argument(
-        "-aI",
+        "-i",
         "--auth-identity",
         dest="auth_identity",
         metavar="USR:PWD",
         help="Authentication credentials (username:password) to be passed down to the specified Authentication Handler.",
     )
     parser.add_argument(
-        "-aU",
+        "-u",
         "--auth-url",
         dest="auth_url",
         help="Authentication URL to be passed down to the specified Authentication Handler.",
     )
     parser.add_argument(
-        "-aM",
+        "-m",
         "--auth-method",
         dest="auth_method",
         metavar="HTTP_METHOD",
@@ -71,23 +66,21 @@ def add_request_options(parser: argparse.ArgumentParser) -> None:
         ),
     )
     parser.add_argument(
-        "-aH",
+        "-H",
         "--auth-header",
         action=ValidateHeaderAction,
         nargs=1,
         dest="auth_headers",
         metavar="HEADER",
         help=(
-            "Additional headers to apply for sending requests when using the authentication handler. "
-            "This option can be provided multiple times, each with a value formatted as:"
-            "\n\n``Header-Name: value``\n\n"
-            "Header names are case-insensitive. "
-            "Quotes can be used in the ``value`` portion to delimit it. "
-            "Surrounding spaces are trimmed."
+            "Additional HTTP headers to include when sending requests via the authentication handler. "
+            "This option may be specified multiple times; each value must be formatted as "
+            "`Header-Name: value`. Header names are case-insensitive; single or double quotes may be used to delimit the value;"
+            "surrounding spaces are trimmed."
         ),
     )
     parser.add_argument(
-        "-aT",
+        "-t",
         "--auth-token",
         dest="auth_token",
         metavar="TOKEN",
